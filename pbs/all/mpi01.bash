ee1d0517e6f708896ae3d3505039ed508b85f927
Pedro Costa <dev@pfac.info>
2012-05-14 17:47:16 +0100

    Added PBS scripts for any nodes (MPI)

---
+++ pbs/all/mpi01.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# PBS Script for the SeARCH Group hex
+# 	Sequential Samplesort
+#
+#PBS -N samplesort-mpi01
+#
+#PBS -l nodes=1
+#PBS -l walltime=30:00
+#
+#PBS -M pdrcosta90@gmail.com
+#PBS -m abe
+#
+#PBS -e ../../data/pbs.out/mpi01.all.err
+#PBS -o ../../data/pbs.out/mpi01.all.out
+#
+#PBS -V
+#
+runs=10;
+cd "$PBS_O_WORKDIR";
+module load gnu/openmpi;
+for (( i = 0 ; i < $runs ; ++i ));
+do
+	mpirun -np 1 -machinefile $PBS_NODEFILE -loadbalance mpi ../../data/input/512MB.txt;
+done;
+
---
+++ pbs/all/mpi02.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# PBS Script for the SeARCH Group hex
+# 	Sequential Samplesort
+#
+#PBS -N samplesort-mpi02
+#
+#PBS -l nodes=2
+#PBS -l walltime=30:00
+#
+#PBS -M pdrcosta90@gmail.com
+#PBS -m abe
+#
+#PBS -e ../../data/pbs.out/mpi02.all.err
+#PBS -o ../../data/pbs.out/mpi02.all.out
+#
+#PBS -V
+#
+runs=10;
+cd "$PBS_O_WORKDIR";
+module load gnu/openmpi;
+for (( i = 0 ; i < $runs ; ++i ));
+do
+	mpirun -np 2 -machinefile $PBS_NODEFILE -loadbalance mpi ../../data/input/512MB.txt;
+done;
+
