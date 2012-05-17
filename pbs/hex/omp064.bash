a867e5c920476fdddeb110afd3ec6bf660d88bb4
Pedro Costa <dev@pfac.info>
2012-05-17 03:50:29 +0100

    New hex scripts

--- pbs/hex/omp064.bash
+++ pbs/hex/omp064.bash
@@ -2,21 +2,21 @@
 # PBS Script for the SeARCH Group hex
 # 	Sequential Samplesort
 #
-#PBS -N samplesort-omp12
+#PBS -N samplesort-omp064
 #
 #PBS -l nodes=1:hex:ppn=24
-#PBS -l walltime=30:00
+#PBS -l walltime=1:00:00
 #
 #PBS -M pdrcosta90@gmail.com
 #PBS -m abe
 #
-#PBS -e ../../data/pbs.out/omp12.hex.err
-#PBS -o ../../data/pbs.out/omp12.hex.out
+#PBS -e ../../data/pbs.out/omp064.hex.err
+#PBS -o ../../data/pbs.out/omp064.hex.out
 #
 runs=10;
 cd "$PBS_O_WORKDIR";
 for (( i = 0 ; i < $runs ; ++i ));
 do
-	./omp ../../data/input/512MB.txt 12;
+	./omp ../../data/input/512MB.txt 64;
 done;
 
--- pbs/hex/omp512.bash
+++ pbs/hex/omp512.bash
@@ -2,21 +2,21 @@
 # PBS Script for the SeARCH Group hex
 # 	Sequential Samplesort
 #
-#PBS -N samplesort-omp01
+#PBS -N samplesort-omp512
 #
 #PBS -l nodes=1:hex:ppn=24
-#PBS -l walltime=30:00
+#PBS -l walltime=1:00:00
 #
 #PBS -M pdrcosta90@gmail.com
 #PBS -m abe
 #
-#PBS -e ../../data/pbs.out/omp01.hex.err
-#PBS -o ../../data/pbs.out/omp01.hex.out
+#PBS -e ../../data/pbs.out/omp512.hex.err
+#PBS -o ../../data/pbs.out/omp512.hex.out
 #
 runs=10;
 cd "$PBS_O_WORKDIR";
 for (( i = 0 ; i < $runs ; ++i ));
 do
-	./omp ../../data/input/512MB.txt 1;
+	./omp ../../data/input/512MB.txt 512;
 done;
 
